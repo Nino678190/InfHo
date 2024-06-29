805e8a7239102171deade612850f836286fd0695
Nino678190 <superwauzi@gmx.de>
2024-06-29 12:08:11 +0200

    Funktionen hinzugefügt zum anmelden und shorten

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 function login(){
     var pw = document.getElementbyID("password");
     var hashed = sha256(pw)
-    var url="192.168.178.105/login"
-    FETCH{
+    fetch("http://192.168.178.105/login"),
+    {
+        method: "POST",
+        credentials:"same-origin",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({password: hashed})
+        .then(function(res){
+            console.log(res)
+            if (res.status == 200){
+                window.location.href="/loggedin.html";
+            }
+            else{
+                alert("Falsches Geheimnis")
+            }
+        })
+        .catch(function(error) {
+            console.error('Error:', error);
+        }),
+    }
+}
 
-         
-    
-    } 
+function urlshort(){
+    var url = document.getElementById("linkeingabe")
+    var ausgabe = document.getElementById("linkausgabe")
+    fetch("http://192.168.178.105/urlshrt"),{
+        method: "POST",
+        credentials: "same-origin",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({url: url})
+        .then(function(res){
+            console.log(res)
+            if (res.status == 200){
+                res.json().then((data) => {
+                    ausgabe.innerHTML(data.shorturl)
+                })
+            }
+            else{
+                alert("Fehler")
+            }
+        })
+        .catch(function(error) {
+            console.error('Error:', error);
+        }),
+    }
 }
