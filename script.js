adc335d640bd859bd20b2bfa8bfa52ed45eda118
Nino678190 <superwauzi@gmx.de>
2024-11-30 12:54:01 +0100

    Einstellungen für VSCode entfernt und API-Endpunkt in script.js aktualisiert

--- script.js
+++ script.js
@@ -25,20 +25,24 @@ function urlshort(){
     if (document.getElementById("linkeingabe").value !== ""){
         var url = document.getElementById("linkeingabe").value
         var ausgabe = document.getElementById("linkausgabe")
-        fetch("http://192.168.178.105\urlshrt",{
+        fetch("http://192.168.178.105/api/shorten",{
             method: "POST",
             credentials: "same-origin",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({url: url})
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                link: url
+            })
         }).then(function(res){
-            console.log(res)
+            console.log(res);
             if (res.status == 200){
                 res.json().then((data) => {
-                    ausgabe.innerHTML(data.shorturl)
-                })
+                    ausgabe.innerHTML(data.shorturl);
+                });
             }
             else{
-                alert("Fehler")
+                alert("Fehler");
             }
         })
         .catch(function(error) {
